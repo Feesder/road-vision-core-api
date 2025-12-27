47784e9cf80cd238bd0bd6bbdad4ced688026938
Yerdos <qop6261@gmail.com>
2025-12-27 22:41:19 +0500

    feat: add config

---
+++ src/common/config/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    db_host: str
+    db_port: int
+    db_user: str
+    db_password: str
+    db_name: str
+    db_sslmode: str
+
+
+settings = Settings()
