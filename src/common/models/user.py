30d713ea6f1c68774a2fe0fbbcec803f6776b1db
Yerdos <qop6261@gmail.com>
2025-12-29 03:54:31 +0500

    feat: add user table uuid

--- src/common/models/user.py
+++ src/common/models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
 
-class User(Base):
+class User(Base, SQLAlchemyBaseUserTableUUID):
     __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(String(255), unique=True)
