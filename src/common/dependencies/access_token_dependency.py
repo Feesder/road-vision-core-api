c05232588e78780358460c137ac5c3b81293f139
Yerdos <qop6261@gmail.com>
2025-12-30 02:37:52 +0500

    feat: add db dependencies

---
+++ src/common/dependencies/access_token_dependency.py
@@ -0,0 +1,9 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.common.database.db_helper import db_helper
+from src.common.models.access_token import AccessToken
+
+async def get_user_db(
+    session: AsyncSession = Depends(db_helper.scoped_session_depedency),
+):
+    yield AccessToken.get_db(session)
---
+++ src/common/dependencies/users_dependency.py
@@ -0,0 +1,9 @@
+from fastapi import Depends
+from src.common.models.user import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.common.database.db_helper import db_helper
+
+async def get_user_db(
+    session: AsyncSession = Depends(db_helper.scoped_session_depedency),
+):
+    yield User.get_db(session)
