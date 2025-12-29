ecf0913a6a00a1006d6376909750383c17c68bc2
Yerdos <qop6261@gmail.com>
2025-12-30 01:35:10 +0500

    feat: update user schema

--- src/common/models/user.py
+++ src/common/models/user.py
@@ -16,4 +16,4 @@ class User(Base, SQLAlchemyBaseUserTableUUID):
 
     @classmethod
     def get_db(cls, session: AsyncSession):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
