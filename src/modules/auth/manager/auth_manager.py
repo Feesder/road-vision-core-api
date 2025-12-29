c7d97543473f1d1534127f8b76655a7b6e37513f
Yerdos <qop6261@gmail.com>
2025-12-30 03:16:07 +0500

    feat: add auth manager

---
+++ src/modules/auth/manager/auth_manager.py
@@ -0,0 +1,27 @@
+import logging
+from typing import Optional
+import uuid
+from fastapi import Request
+from fastapi_users.manager import BaseUserManager, UUIDIDMixin
+from src.common.models.user import User
+from src.config.config import settings
+
+log = logging.getLogger(__name__)
+
+
+class AuthManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = settings.access_toeken_settings.reset_password_token_secret
+    verification_token_secret = settings.access_toeken_settings.verification_token_secret
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        log.warning("User %s has registered.", user.id)
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        log.warning("User %s has forgot their password. Reset token: %s", user.id, token)
+
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        log.warning("Verification requested for user %s. Verification token: %s", user.id, token)    
