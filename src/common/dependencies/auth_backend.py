9ea53cf8dd050ce7baa66fbb803d32cb2217bff5
Yerdos <qop6261@gmail.com>
2025-12-30 02:48:28 +0500

    feat: add auth backend

---
+++ src/common/dependencies/auth_backend.py
@@ -0,0 +1,12 @@
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport
+from src.common.dependencies.strategy import get_database_strategy 
+
+bearer_transport = BearerTransport(
+    tokenUrl="/api/v1/auth/sign-in",
+)
+
+authentication_backend = AuthenticationBackend(
+    name="access-tokens-db",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
