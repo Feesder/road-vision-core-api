baba00e9ecf59030622258a6b75feb2885364986
Yerdos <qop6261@gmail.com>
2025-12-30 01:36:38 +0500

    feat: create access_token schema

---
+++ src/common/models/access_token.py
@@ -0,0 +1,18 @@
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
+from src.common.models.base import Base
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import GUID
+from sqlalchemy import ForeignKey
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
+
+class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):
+    __tablename__ = "access_tokens"
+
+    user_id: Mapped[str] = mapped_column(GUID, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyAccessTokenDatabase(session, cls)
