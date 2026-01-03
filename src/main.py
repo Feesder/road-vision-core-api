5da1fd403ec9667d58383b5194141228666b157e
Yerdos <qop6261@gmail.com>
2026-01-03 12:29:11 +0500

    feat: create road_condition_features route

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, Depends
 
 from src.modules.auth.presenter.auth_controller import auth_router
 from src.modules.users.presenter.users_controller import users_router
-
+from src.modules.road_condition_features.presenter.road_condition_feature_controller import (
+    road_condition_feature_router,
+)
 from fastapi.security import HTTPBearer
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -10,6 +12,7 @@ http_bearer = HTTPBearer(auto_error=False)
 app = FastAPI(title="Road Vision Core API", dependencies=[Depends(http_bearer)])
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(road_condition_feature_router)
 
 
 @app.get("/")
